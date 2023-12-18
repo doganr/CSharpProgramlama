b2f1b9733fd6ab637e87d73c7eea886543da20de
Ramazan Özgür DOĞAN <dogan@outlook.com>
2023-12-18 14:50:57 +0300

    Hafta12 dersi, eklendi...

---
+++ Hafta12/Ogrenci.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hafta12
+{
+    public enum GecmeKalma{ Gecti, SartliGecti, Kaldi}
+    public class Ogrenci
+    {
+        private string _numara;
+        private string _ad;
+        private string _soyad;
+        private int _yas;
+        private Cinsiyet _cinsiyet;
+        private double _vize;
+        private double _final;
+
+        public string Numara
+        {
+            get { return _numara; }
+        }
+
+        public string Ad
+        {
+            get { return _ad; }
+        }
+
+        public string Soyad 
+        {
+            get { return _soyad; }
+        }  
+
+        public int Yas 
+        {
+            get { return _yas; }
+            set { _yas = value<0 ? 0 : value; }
+        }
+
+        public Cinsiyet Cinsiyet 
+        {
+            get { return _cinsiyet; }
+            set { _cinsiyet = value;}
+        }
+
+        public double Vize 
+        {
+            get { return _vize; }
+            set 
+            { 
+                if (value<0)
+                    _vize = 0;
+                else if(value>100)
+                    _vize = 100;
+                else
+                    _vize = value;            
+            }
+        }
+
+        public double Final
+        {
+            get { return _final; }
+            set
+            {
+                if (value < 0)
+                    _final = 0;
+                else if (value > 100)
+                    _final = 100;
+                else
+                    _final = value;
+            }
+        }
+
+        public double Ortalama 
+        {
+            get 
+            {
+                double ortalama = _vize * 0.4 + _final * 0.6;
+                return ortalama; 
+            }
+        }
+
+        public GecmeKalma GecmeDurumu 
+        {
+            get 
+            {
+                if (_final < 45)
+                    return GecmeKalma.Kaldi;
+                else if (Ortalama >= 45 && Ortalama < 50)
+                    return GecmeKalma.SartliGecti;
+                else
+                    return GecmeKalma.Gecti;
+            }
+        }
+
+        public string HarfNotu 
+        {
+            get 
+            {
+                if (GecmeDurumu == GecmeKalma.Kaldi)
+                    return "FF";
+                else 
+                {
+                    if (Ortalama <= 100 && Ortalama >= 80.5)
+                        return "AA";
+                    else if (Ortalama < 80.5 && Ortalama >= 75.5)
+                        return "BA";
+                    else if (Ortalama < 75.5 && Ortalama >= 69.5)
+                        return "BB";
+                    else if (Ortalama < 69.5 && Ortalama >= 59.5)
+                        return "CB";
+                    else if (Ortalama < 59.5 && Ortalama >= 49.5)
+                        return "CC";
+                    else if (Ortalama < 49.5 && Ortalama >= 44.5)
+                        return "DC";
+                    else if (Ortalama < 44.5 && Ortalama >= 39.5)
+                        return "DD";
+                    else if (Ortalama < 39.5 && Ortalama >= 29.5)
+                        return "FD";
+                    else
+                        return "FF";
+                }                
+            }
+        }        
+
+        public Ogrenci(string numara, string ad, string soyad) 
+        {
+            this._numara = numara;
+            this._ad = ad;
+            this._soyad = soyad;
+        }
+
+        public List<object> DataRow 
+        {
+            get{
+                return new List<object>() { this.Numara,
+                                        this.Ad,
+                                        this.Soyad,
+                                        this.Yas,
+                                        this.Cinsiyet,
+                                        this.Vize,
+                                        this.Final,
+                                        this.Ortalama,
+                                        this.HarfNotu,
+                                        this.GecmeDurumu};
+            }
+        }
+    }
+}
---
+++ Hafta12/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Runtime.Serialization;
+using System.Text;
+using System.Text.Json;
+
+namespace Hafta12
+{
+    public enum Cinsiyet { Kadin, Erkek }
+
+    public class Program
+    {
+        static void Main()
+        {
+            List<Ogrenci> ogrenciler = new List<Ogrenci>();
+
+            ogrenciler.Add(new Ogrenci("2207231061", "Sinan", "Turan") { Yas = 19, Cinsiyet = Cinsiyet.Erkek });
+            ogrenciler.Add(new Ogrenci("2207231032", "Ahmet", "Yavuz") { Yas = 20, Cinsiyet = Cinsiyet.Erkek });
+            ogrenciler.Add(new Ogrenci("2207231055", "Burçin", "Lale") { Yas = 21, Cinsiyet = Cinsiyet.Kadin });
+
+            ogrenciler[0].Vize = 98;
+            ogrenciler[0].Final = 55;
+
+
+
+            OgrenciDataFrame tablo = new OgrenciDataFrame();
+
+            foreach (Ogrenci ogr in ogrenciler)
+            {
+                tablo.addRow(ogr.DataRow);
+            }
+            tablo.PrintTable();
+
+            string data = JsonSerializer.Serialize<List<Ogrenci>>(ogrenciler);
+
+            File.WriteAllText("ogrenciler.json", data, Encoding.UTF8);
+
+            string okunan = File.ReadAllText("ogrenciler.json", Encoding.UTF8);
+
+            List<Ogrenci> ogrenciler2 = JsonSerializer.Deserialize<List<Ogrenci>>(okunan);
+        }
+    }
+}
